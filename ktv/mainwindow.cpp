f4da6ce492c4c8f9d1e76ca812bc928d125c13f8
Sarith Khlot <skhlot@sitscape.com>
2015-11-11 12:49:11 -0500

    test mplayer

--- ktv/mainwindow.cpp
+++ ktv/mainwindow.cpp
@@ -1,14 +1,36 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <string>
+#include <QMutex>
+#include <QWaitCondition>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QString command = "mplayer /home/sarith/Downloads/song1.mp4";
+    //sleep(5);
+    //system("mkfifo /tmp/mplayer-control");
+    system("mplayer -slave -fs -input file=/tmp/mplayer-control /home/sarith/Videos/jerm_song.DAT");
+    //msleep(100);
+    system("echo \"af pan=2:1:1:0:0\" > /tmp/mplayer-control");
+    //system(qPrintable(command));
+    //ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::msleep(unsigned long msecs)
+{
+    QMutex mutex;
+    mutex.lock();
+
+    QWaitCondition waitCondition;
+    waitCondition.wait(&mutex, msecs);
+
+    mutex.unlock();
 }
--- ktv/mainwindow.h
+++ ktv/mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void msleep(unsigned long msecs);
 
 private:
     Ui::MainWindow *ui;
