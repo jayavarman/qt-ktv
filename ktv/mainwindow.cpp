040f43b7ba466d83cc3997ba66edcb2cd4188056
Sarith Khlot <skhlot@sitscape.com>
2015-11-12 15:38:25 -0500

    test with mplayer process

--- ktv/mainwindow.cpp
+++ ktv/mainwindow.cpp
@@ -3,20 +3,30 @@
 #include <string>
 #include <QMutex>
 #include <QWaitCondition>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QString command = "mplayer /home/sarith/Downloads/song1.mp4";
+    //QString command = "mplayer /home/sarith/Downloads/song1.mp4";
     //sleep(5);
-    system("mkfifo /tmp/mplayer-control");
-    system("mplayer -slave -fs -input file=/tmp/mplayer-control /home/sarith/Videos/jerm_song.DAT -af pan=2:1:1:0:0");
+    //system("mkfifo /tmp/mplayer-control");
+    //system("mplayer -slave -fs -playlist /home/sarith/Videos/playlist.txt -input file=/tmp/mplayer-control -af pan=2:1:1:0:0");
+    //system("mplayer -slave -input file=/tmp/mplayer-control /home/sarith/Videos/jerm_song.DAT -af pan=2:1:1:0:0");
     //msleep(100);
     //system("echo \"af pan=2:1:1:0:0\" > /tmp/mplayer-control");
     //system(qPrintable(command));
     //ui->setupUi(this);
+
+    mplayer_proc = new QProcess( parent );
+    connect( mplayer_proc, SIGNAL( finished(int) ), this, SLOT( exitedMPlayer(int) ) );
+    //connect( mplayer_proc, SIGNAL( wroteToStdin () ), this, SLOT( wroteToStdinMPlayer() ) );
+    //connect( mplayer_proc, SIGNAL( readyReadStderr () ), this, SLOT( readyReadStderrMPlayer() ) );
+    //connect( mplayer_proc, SIGNAL( readyReadStdout() ), this, SLOT( readStdoutMPlayer() ) );
+    mplayer_proc->start("mplayer -slave -fs -input file=/tmp/mplayer-control /home/sarith/Videos/song1.mp4");
+
 }
 
 MainWindow::~MainWindow()
@@ -33,4 +43,9 @@ void MainWindow::msleep(unsigned long msecs)
     waitCondition.wait(&mutex, msecs);
 
     mutex.unlock();
+}
+
+void MainWindow::exitedMPlayer(int exitCode) {
+    qDebug() << "MPlayer exited." << exitCode;
+    mplayer_proc->start("mplayer -slave -fs -input file=/tmp/mplayer-control /home/sarith/Videos/jerm_song.DAT");
 }
--- ktv/mainwindow.h
+++ ktv/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProcess>
 
 namespace Ui {
 class MainWindow;
@@ -12,12 +13,16 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    QProcess *mplayer_proc;
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     void msleep(unsigned long msecs);
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void exitedMPlayer(int exitCode);
 };
 
 #endif // MAINWINDOW_H
